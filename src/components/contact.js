0984dbda55d840a79e28b7e97001978268ebfaac
Amos Cohen <mr.amoscohen@gmail.com>
2023-08-05 13:41:50 +0300

    Fixing stuff v2

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,11 +13,21 @@ function MyContact(props) {
                 <h3 style={{marginBottom: '10px'}}><i className="fa-regular fa-comment" style={{marginRight:"10px"}}></i>Contact</h3>
             </Grid>
             <Grid container spacing={1.5} direction="column">
-                <Grid item xs={12}>
-                <p><i className="fa-solid fa-envelope" style={{marginRight: "2px"}}></i>{props.contact.email}</p>
+                <Grid container item xs={12} alignItems="center" spacing={0.5}>
+                    <Grid item sx={{marginRight: "2px"}}>
+                    <i className="fa-solid fa-envelope"></i>
+                    </Grid>
+                    <Grid item>
+                    <p>{props.contact.email}</p>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                <p><i className="fa-solid fa-phone" style={{marginRight: "2px"}}></i>{props.contact.phone}</p>        
+                <Grid container item xs={12} alignItems="center" spacing={0.5}>
+                    <Grid item sx={{marginRight: "2px"}}>
+                    <i className="fa-solid fa-phone"></i>
+                    </Grid>
+                    <Grid item>
+                    <p>{props.contact.phone}</p> 
+                    </Grid>     
                 </Grid>
             </Grid>
        
